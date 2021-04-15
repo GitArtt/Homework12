5dee06a6ceca47465e650d28a889ae119a87059f
geekbrainsArtem <magnum.rus@inbox.ru>
2021-04-15 18:41:22 +0300

    dev branch

--- src/main/java/ru/geekbrains/homework12/Main.java
+++ src/main/java/ru/geekbrains/homework12/Main.java
@@ -3,8 +3,8 @@ package ru.geekbrains.homework12;
 import java.util.Arrays;
 
 public class Main {
-    static final int size = 10000000;
-    static final int half = size / 2;
+    private static final int size = 10000000;
+    private static final int half = size / 2;
 
     public static void main(String[] args) {
         float[] arr1 = createArray(size);
@@ -14,7 +14,7 @@ public class Main {
         System.out.println("arr1 & arr2 equals: " + Arrays.equals(arr1, arr2));
     }
 
-    static void firstMethod(float[] arr) {
+    private static void firstMethod(float[] arr) {
         long a = System.currentTimeMillis();
         countIt(arr);
         System.out.println((System.currentTimeMillis() - a) / 1000.0 + "s");
@@ -26,7 +26,7 @@ public class Main {
         return arr;
     }
 
-    static void secondMethod(float[] arr) {
+    private static void secondMethod(float[] arr) {
         float[] a1 = new float[half];
         float[] a2 = new float[half];
         System.arraycopy(arr, 0, a1, 0, half);
@@ -49,13 +49,13 @@ public class Main {
         System.out.println("Две части одного массива a1 & a2 equals: " + Arrays.equals(a1, a2));
     }
 
-    static void countIt(float[] arr) {
+    private static void countIt(float[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
     }
 
-    static void countIt(float[] arr, int offset) {
+    private static void countIt(float[] arr, int offset) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + offset) / 5) *
                     Math.cos(0.2f + (i + offset) / 5) * Math.cos(0.4f + (i + offset) / 2));
